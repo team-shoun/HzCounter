4376cc1b191a2ab9733f5e511d30fcfcf37f23b5
maluyuu <yuya.maruoka@outlook.jp>
2024-10-27 23:21:17 +0900

    多分完成

--- HzCount/HzCount.ino
+++ HzCount/HzCount.ino
@@ -1,14 +1,58 @@
 #define analogPin 1;
+#include <Adafruit_SSD1306.h>  //OLED用ライブラリ
+#define SCREEN_WIDTH 128       // OLEDディスプレイの幅（ピクセル単位）
+#define SCREEN_HEIGHT 32       // OLEDディスプレイの高さ（ピクセル単位）
 
 int startMillis;
 int count;
 
-void setup() {
-  pinMode(analogPin,INPUT);
+int threshold = 512;
 
+void displaySet() {
+  display.clearDisplay();               // Clear the buffer
+  display.setTextSize(1);               // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);  // Draw white text
+  display.setCursor(0, 0);              // Start at top-left corner
+}
+
+void starting() {
+  displaySet();
+  display.println("Hz Countor");
+  display.println("made by");
+  display.println("Robotics");
+  display.display();
+  delay(1000);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(analogPin, INPUT);
+  //SSD1306_SWITCHCAPVCC = 内部で3.3Vからディスプレイ電圧を生成する
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 の割り当てに失敗しました"));
+    for (;;)
+      ;  //先に進まないで、永遠にループする
+  }
+  Serial.println("OLED OK...");
+  display.clearDisplay();  //バッファをクリアする
+  display.display();
+  starting();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  startMillis = millis();
+  count = 0 while (millis() - startMillis < 1000) {
+    while (analogRead(analogPin) > threshold) {
+      delay(0);
+    }
+    while (analogRead(analogPin) < threshold) {
+      delay(0);
+    }
+    count += 1;
+  }
+  float Hz = (count / time) * 1000;  //1秒あたりの振動数
+  displaySet();
+  display.print(Hz);
+  display.print(" Hz");
+  display.display();
 }
